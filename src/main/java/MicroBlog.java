19d4ba0a0b1e61a1143a287d13c1e018248c2abd
Maciej Wojcik <s6554@horyzont.eu>
2020-12-19 02:21:44 +0100

    task 1.4 commit

--- src/main/java/MicroBlog.java
+++ src/main/java/MicroBlog.java
@@ -1,12 +1,21 @@
 import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.log4j.BasicConfigurator;
-//import org.apache.log4j.Logger;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
 
 public class MicroBlog {
 
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
-		get("/hello", (req, res) -> "Hello World");
+		
+		get("/hello", (reqest, response) -> {
+			Map<String, Object> attributes = new HashMap<>();
+			attributes.put("message", "Hello World");
+			
+			return new ModelAndView(attributes, "hello.ftl");
+		}, new FreeMarkerEngine());
 	}
 
 }
