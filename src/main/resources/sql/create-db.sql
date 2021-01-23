294a89a2b243547885365b8bf295aace9fcb33ad
unknown <jack.pawelek@gmail.com>
2021-01-23 17:14:05 +0100

    Updated creat-db file, 3 tables are done and added

--- src/main/resources/sql/create-db.sql
+++ src/main/resources/sql/create-db.sql
@@ -1,19 +1,33 @@
-drop schema public cascade ;
-drop table User if exists ;
-create table User (
-	UserID INTEGER NOT NULL PRIMARY KEY,
-	UserName VARCHAR(255),
-	UserPassword VARCHAR(255)
+drop schema public cascade;
+
+drop table if exists users;
+CREATE TABLE `users` (
+ `userId` bigint(20) NOT NULL AUTO_INCREMENT,
+ `userName` varchar(60) NOT NULL,
+ `userPass` varchar(60) NOT NULL,
+ PRIMARY KEY (`userId`)
 );
 
+drop table if exists posts;
+CREATE TABLE `posts` (
+ `postId` bigint(20) NOT NULL AUTO_INCREMENT,
+ `content` longtext NOT NULL,
+ `date` datetime NOT NULL,
+ `title` varchar(255) NOT NULL,
+ `userId` bigint(20) NOT NULL,
+ PRIMARY KEY (`postId`),
+ KEY `userId` (`userId`),
+ CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
+);
 
-/*
-drop table follower if exists ;
-create table follower (
-….
+drop table if exists followers;
+CREATE TABLE `followers` (
+ `followerId` bigint(20) NOT NULL AUTO_INCREMENT,
+ `userId` bigint(20) NOT NULL,
+ `follows` bigint(20) NOT NULL,
+ PRIMARY KEY (`followerId`),
+ KEY `userId` (`userId`),
+ KEY `follows` (`follows`),
+ CONSTRAINT `followers_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`),
+ CONSTRAINT `followers_ibfk_2` FOREIGN KEY (`follows`) REFERENCES `users` (`userId`)
 );
-drop table post if exists ;
-create table post (
-…..
-);
-*/
