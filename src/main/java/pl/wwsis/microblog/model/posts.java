7ba4a9a7a1c7ab2bd28082666884d2e79fa8c389
unknown <jack.pawelek@gmail.com>
2021-01-24 18:05:15 +0100

    data source file divided into 3 classes-tables

---
+++ src/main/java/pl/wwsis/microblog/model/posts.java
@@ -0,0 +1,5 @@
+package pl.wwsis.microblog.model;
+
+public class posts {
+
+}
--- target/classes/sql/create-db.sql
+++ target/classes/sql/create-db.sql
@@ -1,33 +1,29 @@
 drop schema public cascade;
 
-drop table if exists users;
-CREATE TABLE `users` (
- `userId` bigint(20) NOT NULL AUTO_INCREMENT,
- `userName` varchar(60) NOT NULL,
- `userPass` varchar(60) NOT NULL,
- PRIMARY KEY (`userId`)
+drop table users if exists;
+CREATE TABLE users (
+ userId bigint NOT NULL PRIMARY KEY ,
+ userName varchar(255) NOT NULL,
+ userPass varchar(255) NOT NULL,
 );
 
-drop table if exists posts;
-CREATE TABLE `posts` (
- `postId` bigint(20) NOT NULL AUTO_INCREMENT,
- `content` longtext NOT NULL,
- `date` datetime NOT NULL,
- `title` varchar(255) NOT NULL,
- `userId` bigint(20) NOT NULL,
- PRIMARY KEY (`postId`),
- KEY `userId` (`userId`),
- CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
+drop table posts if exists;
+CREATE TABLE posts (
+ postId bigint NOT NULL ,
+ content LONGVARCHAR NOT NULL,
+ date datetime NOT NULL,
+ title varchar(255) NOT NULL,
+ userId bigint NOT NULL,
+ PRIMARY KEY (postId),
+ CONSTRAINT posts_ibfk_1 FOREIGN KEY (userId) REFERENCES users
 );
 
-drop table if exists followers;
-CREATE TABLE `followers` (
- `followerId` bigint(20) NOT NULL AUTO_INCREMENT,
- `userId` bigint(20) NOT NULL,
- `follows` bigint(20) NOT NULL,
- PRIMARY KEY (`followerId`),
- KEY `userId` (`userId`),
- KEY `follows` (`follows`),
- CONSTRAINT `followers_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`),
- CONSTRAINT `followers_ibfk_2` FOREIGN KEY (`follows`) REFERENCES `users` (`userId`)
+drop table followers if exists ;
+CREATE TABLE followers (
+ followerId bigint NOT NULL ,
+ userId bigint NOT NULL,
+ follows bigint NOT NULL,
+ PRIMARY KEY (followerId),
+ CONSTRAINT followers_ibfk_1 FOREIGN KEY (userId) REFERENCES users,
+ CONSTRAINT followers_ibfk_2 FOREIGN KEY (follows) REFERENCES users
 );
