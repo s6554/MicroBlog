c0f16141a5ae9c0c249f5d6f11bac074b4a3c826
Konrad Pilarczyk <73016214+kodzio-wln@users.noreply.github.com>
2021-01-25 23:37:21 +0100

    update java db files

--- src/main/java/pl/wwsis/microblog/model/followers.java
+++ src/main/java/pl/wwsis/microblog/model/followers.java
@@ -1,5 +1,24 @@
 package pl.wwsis.microblog.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 public class followers {
 
+	@Entity
+	@Table (name="followers")
+	public class Followers{
+		
+		@Column (name= "followerId" , nullable= true )
+		private int followerId;
+	
+		@Column (name= "userId" , nullable= true )
+		private int userId;
+		
+		@Column (name= "follows" , nullable= true )
+		private int follows;
+
+	}
+	
 }
--- src/main/java/pl/wwsis/microblog/model/posts.java
+++ src/main/java/pl/wwsis/microblog/model/posts.java
@@ -1,5 +1,30 @@
 package pl.wwsis.microblog.model;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 public class posts {
 
+	@Entity
+	@Table (name="posts")
+	public class Posts{
+		
+		@Column (name= "postId" , nullable= true )
+		private int postId;
+		
+		@Column (name= "content" , nullable= false )
+		private String userName;
+		
+		@Column (name= "date" , nullable= false )
+		private Date date;
+		
+		@Column (name= "title" , nullable= false )
+		private String title;
+		
+		@Column (name= "userId" , nullable= true )
+		private int userId;
+	}
 }
