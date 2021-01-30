f928e1062cd46ce41a9601f4df452c1aa5203891
Maciej Wojcik <s6554@horyzont.eu>
2021-01-30 12:41:13 +0100

    dao interface corrected

--- src/main/java/pl/wwsis/microblog/dao/PostsDao.java
+++ src/main/java/pl/wwsis/microblog/dao/PostsDao.java
@@ -2,15 +2,14 @@ package pl.wwsis.microblog.dao;
 
 import java.sql.Date;
 import java.util.List;
-import java.util.Map;
 
 public interface PostsDao<T, S> { //generic types for posts and users
 	
 	List<T> getUsersTimeline(S user); //returns all posts of specific user
 	
-	Map<T, S> getUsersFullTimeline(S loggedUser); //returns all posts of logged user and others' that he follows
+	List<T> getUsersFullTimeline(S loggedUser); //returns all posts of logged user and others' that he follows
 	
-	Map<T, S> getFullPublicTimeline(); //returns all posts of all users
+	List<T> getFullPublicTimeline(); //returns all posts of all users
 	
 	void addPost(String content, Date date, String title, long userId); //adds new post
 }
